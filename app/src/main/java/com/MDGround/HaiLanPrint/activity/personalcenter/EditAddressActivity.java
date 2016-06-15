95da45bcf646996abd5bee51d6e784e96f403ff4
vic5816 <836380339@qq.com>
2016-06-15 15:39:44 +0800

    地址管理

--- app/src/main/java/com/MDGround/HaiLanPrint/activity/personalcenter/EditAddressActivity.java
+++ app/src/main/java/com/MDGround/HaiLanPrint/activity/personalcenter/EditAddressActivity.java
@@ -1,40 +1,123 @@
 package com.MDGround.HaiLanPrint.activity.personalcenter;
 
 import android.content.Intent;
+import android.view.View;
 
 import com.MDGround.HaiLanPrint.R;
 import com.MDGround.HaiLanPrint.activity.base.ToolbarActivity;
+import com.MDGround.HaiLanPrint.application.MDGroundApplication;
 import com.MDGround.HaiLanPrint.databinding.ActivityEditorAddressBinding;
+import com.MDGround.HaiLanPrint.enumobject.restfuls.ResponseCode;
+import com.MDGround.HaiLanPrint.greendao.Location;
 import com.MDGround.HaiLanPrint.models.DeliveryAddress;
+import com.MDGround.HaiLanPrint.restfuls.GlobalRestful;
+import com.MDGround.HaiLanPrint.restfuls.bean.ResponseData;
+import com.MDGround.HaiLanPrint.utils.StringUtil;
+import com.MDGround.HaiLanPrint.utils.ViewUtils;
+import com.MDGround.HaiLanPrint.views.dialog.RegionPickerDialog;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by PC on 2016-06-14.
  */
-public class EditAddressActivity extends ToolbarActivity<ActivityEditorAddressBinding>{
+public class EditAddressActivity extends ToolbarActivity<ActivityEditorAddressBinding> {
+    DeliveryAddress address;
+    private RegionPickerDialog mRegionPickerDialog;
+    boolean isUpdate = false;
     @Override
     protected int getContentLayout() {
         return R.layout.activity_editor_address;
     }
-
     @Override
     protected void initData() {
-        Intent intent=getIntent();
-        String fromWhere=intent.getStringExtra(ManageAddressActivity.FROM_HERE);
-        if(fromWhere.equals(ManageAddressActivity.FOR_UPATE)){
-            DeliveryAddress address=intent.getParcelableExtra(ManageAddressActivity.ADDRESS);
-           // mDataBinding.etAddress.setText(address.getCityID());
+        Intent intent = getIntent();
+        mRegionPickerDialog = new RegionPickerDialog(this);
+        int fromWhere = intent.getIntExtra(ManageAddressActivity.FROM_HERE, 0);
+        if (fromWhere == (ManageAddressActivity.FOR_UPATE)) {
+            isUpdate = true;
+            address = intent.getParcelableExtra(ManageAddressActivity.ADDRESS);
             mDataBinding.etContacts.setText(address.getPhone());
             mDataBinding.etReciever.setText(address.getReceiver());
-           mDataBinding.etRegio.setText(address.getStreet());
-//            DeliveryAddress address=bundle.get()
-        }else{
+            mDataBinding.etAddress.setText(address.getStreet());
+            Location province = MDGroundApplication.mDaoSession.getLocationDao().load((long) address.getProvinceID());
+            Location city = MDGroundApplication.mDaoSession.getLocationDao().load((long) address.getCityID());
+            Location country = MDGroundApplication.mDaoSession.getLocationDao().load((long) address.getCountryID());
+            mDataBinding.etRegio.setText(province.getLocationName() + "" + city.getLocationName() + "" + country.getLocationName());
 
+        } else {
+            isUpdate = false;
+            tvTitle.setText("新增地址");
+            address = new DeliveryAddress();
         }
 
     }
 
     @Override
     protected void setListener() {
+        mDataBinding.tvAddAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                address.setReceiver(mDataBinding.etReciever.getText().toString());
+                address.setUserID(MDGroundApplication.mLoginUser.getUserID());
+                if (StringUtil.isEmpty(mDataBinding.etContacts.getText().toString())) {
+                    ViewUtils.toast(R.string.input_phone_number);
+                    return;
+                }
+                if (mDataBinding.etContacts.getText().toString().length() != 11) {
+                    ViewUtils.toast(R.string.input_corrent_phone);
+                    return;
+                }
+                address.setPhone(mDataBinding.etContacts.getText().toString());
+                if (StringUtil.isEmpty(mDataBinding.etAddress.getText().toString())) {
+                    ViewUtils.toast(R.string.input_detailed_address);
+                    return;
+                }
+                address.setStreet(mDataBinding.etAddress.getText().toString());
+                addOrdeleteAddress(address);
+            }
+        });
+        mRegionPickerDialog.setOnRegionSelectListener(new RegionPickerDialog.OnRegionSelectListener() {
+            @Override
+            public void onRegionSelect(Location province, final Location city, final Location county) {
+                address.setProvinceID(Integer.parseInt(String.valueOf(province.getLocationID())));
+                address.setCityID(Integer.parseInt(String.valueOf(city.getLocationID())));
+                address.setCountryID(Integer.parseInt(String.valueOf(county.getLocationID())));
+                mDataBinding.etRegio.setText(province.getLocationName() + "" + city.getLocationName() + " " + county.getLocationName());
+            }
+
+        });
 
     }
+
+    //region SERVER
+    public void addOrdeleteAddress(final DeliveryAddress address) {
+        GlobalRestful.getInstance().SaveUserAddress(address, new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                if (ResponseCode.isSuccess(response.body())) {
+                    if (isUpdate) {
+                        ViewUtils.toast("修改成功");
+                    } else {
+                        ViewUtils.toast("添加成功");
+                    }
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+            }
+        });
+
+    }
+
+    //endgion
+    //region ACTION
+    public void selectRegio(View view) {
+        mRegionPickerDialog.show();
+    }
+    //endregion
 }
--- app/src/main/java/com/MDGround/HaiLanPrint/activity/personalcenter/ManageAddressActivity.java
+++ app/src/main/java/com/MDGround/HaiLanPrint/activity/personalcenter/ManageAddressActivity.java
@@ -16,7 +16,9 @@ import com.MDGround.HaiLanPrint.enumobject.restfuls.ResponseCode;
 import com.MDGround.HaiLanPrint.models.DeliveryAddress;
 import com.MDGround.HaiLanPrint.restfuls.GlobalRestful;
 import com.MDGround.HaiLanPrint.restfuls.bean.ResponseData;
+import com.MDGround.HaiLanPrint.utils.Bookends;
 import com.MDGround.HaiLanPrint.utils.ViewUtils;
+import com.socks.library.KLog;
 
 import java.util.ArrayList;
 
@@ -28,14 +30,16 @@ import retrofit2.Response;
  * Created by PC on 2016-06-14.
  */
 public class ManageAddressActivity extends ToolbarActivity<ActivityManageAddressBinding> {
-    public static final String FROM_HERE="FROM_HERE";
-    public static final  String ADDRESS="ADDRESS";
-    public static final int FOR_UPATE=1;
-    public static final  int FOR_SAVE=2;
-    private ArrayList<DeliveryAddress> mUserAddressList=new ArrayList<>();
+    public static final String FROM_HERE = "FROM_HERE";
+    public static final String ADDRESS = "ADDRESS";
+    public static final int FOR_UPATE = 1;
+    public static final int FOR_SAVE = 2;
+    private ArrayList<DeliveryAddress> mUserAddressList = new ArrayList<>();
     private DeliveryAddress mUserAddress;
-    private  ManageAddressAdatper adatper;
-    private View view;
+    private ManageAddressAdatper adatper;
+    private View footView;
+    private Bookends<ManageAddressAdatper> bookends;
+
     @Override
     protected int getContentLayout() {
         return R.layout.activity_manage_address;
@@ -44,108 +48,142 @@ public class ManageAddressActivity extends ToolbarActivity<ActivityManageAddress
     @Override
     protected void onResume() {
         super.onResume();
+        getUserAddressListRequest();
     }
 
     @Override
     protected void initData() {
-        LinearLayoutManager  linearLayoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mDataBinding.recyclerView.setLayoutManager(linearLayoutManager);
-        View view=LayoutInflater.from(this).inflate(R.layout.item_address_foot,null);
-        adatper=new ManageAddressAdatper();
-        mDataBinding.recyclerView.setAdapter(adatper);
-//        adatper.addFooter()
+        adatper = new ManageAddressAdatper();
+        bookends = new Bookends<>(adatper);
+        footView = LayoutInflater.from(this).inflate(R.layout.item_address_foot, null);
+        bookends.addFooter(footView);
         getUserAddressListRequest();
-
+        mDataBinding.recyclerView.setAdapter(bookends);
     }
+
     //region SERVER
     private void getUserAddressListRequest() {
+        ViewUtils.loading(this);
         GlobalRestful.getInstance().GetUserAddressList(new Callback<ResponseData>() {
             @Override
             public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
-                mUserAddressList=response.body().getContent(new com.google.gson.reflect.TypeToken<ArrayList<DeliveryAddress>>(){});
+                mUserAddressList = response.body().getContent(new com.google.gson.reflect.TypeToken<ArrayList<DeliveryAddress>>() {
+                });
+                bookends.notifyDataSetChanged();
                 adatper.notifyDataSetChanged();
+                ViewUtils.dismiss();
             }
 
             @Override
             public void onFailure(Call<ResponseData> call, Throwable t) {
-
+                ViewUtils.toast("请求失败");
+                ViewUtils.dismiss();
             }
         });
     }
-   //endregion
+
+    //endregion
     @Override
     protected void setListener() {
+        footView.findViewById(R.id.tv_addAddress).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManageAddressActivity.this, EditAddressActivity.class);
+                intent.putExtra(FROM_HERE, FOR_SAVE);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //region ACTION
+    public void onEidtorItem(View view) {
+        int position = mDataBinding.recyclerView.getChildAdapterPosition(view);
+        DeliveryAddress deliveryAddress = mUserAddressList.get(position);
+        KLog.e("第几个" + position);
+        updateAddress(deliveryAddress);
 
     }
 
-    public class BindingHandler{
-        public  void onEidtorItem(View view){
-//            int position = mDataBinding.recyclerView.getChildAdapterPosition(view);
-//            DeliveryAddress deliveryAddress=mUserAddressList.get(position);
+    public void onDeleteItem(View view) {
 
-        }
-        public void onDeleteItem(View view){
-//            int position = mDataBinding.recyclerView.getChildAdapterPosition(view);;
-//            DeliveryAddress address=mUserAddressList.get(position);
-//            int AutoID=address.getAutoID();
-//            deleteAddress(AutoID);
-        }
-
+        int position = mDataBinding.recyclerView.getChildAdapterPosition(view);
+        ;
+        DeliveryAddress address = mUserAddressList.get(position);
+        KLog.e("第几个" + position);
+        int AutoID = address.getAutoID();
+        deleteAddress(AutoID, position, view);
     }
-    BindingHandler handler=new BindingHandler();
+
+    //endregion
     //region ADAPTER
-       public class ManageAddressAdatper extends RecyclerView.Adapter<ManageAddressAdatper.MyViewHolder>{
+    public class ManageAddressAdatper extends RecyclerView.Adapter<ManageAddressAdatper.MyViewHolder> {
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view= LayoutInflater.from(ManageAddressActivity.this).inflate(R.layout.item_manage_address,parent,false);
-            MyViewHolder holder=new MyViewHolder(view);
+            View view = LayoutInflater.from(ManageAddressActivity.this).inflate(R.layout.item_manage_address, parent, false);
+            MyViewHolder holder = new MyViewHolder(view);
             return holder;
         }
 
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position) {
-            DeliveryAddress address=mUserAddressList.get(position);
+            DeliveryAddress address = mUserAddressList.get(position);
             viewItemBinding.setAddress(address);
-            viewItemBinding.setHandler(handler);
-
         }
 
         @Override
         public int getItemCount() {
             return mUserAddressList.size();
         }
-             public  ItemManageAddressBinding viewItemBinding;
-        class  MyViewHolder extends RecyclerView.ViewHolder{
-              public MyViewHolder(View itemView) {
-                  super(itemView);
-                  viewItemBinding= DataBindingUtil.bind(itemView);
-              }
-          }
+
+        public ItemManageAddressBinding viewItemBinding;
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            public MyViewHolder(final View itemView) {
+                super(itemView);
+                viewItemBinding = DataBindingUtil.bind(itemView);
+                viewItemBinding.tvDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onDeleteItem(itemView);
+                    }
+                });
+                viewItemBinding.tvEditor.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onEidtorItem(itemView);
+                    }
+                });
+            }
+        }
     }
+
     //enregion SERVER
     //编辑地址
-    public void updateAddress(DeliveryAddress address){
-       Intent intent=new Intent(this,EditAddressActivity.class);
-        intent.putExtra(FROM_HERE,FOR_UPATE);
-        intent.putExtra(ADDRESS,address);
+    public void updateAddress(DeliveryAddress address) {
+        Intent intent = new Intent(this, EditAddressActivity.class);
+        intent.putExtra(FROM_HERE, FOR_UPATE);
+        intent.putExtra(ADDRESS, address);
         startActivity(intent);
     }
+
     //删除地址
-    public void  deleteAddress(int AutoID){
-        ViewUtils.loading(this);
+    public void deleteAddress(int AutoID, final int postion, final View view) {
         GlobalRestful.getInstance().DeleteUserAddress(AutoID, new Callback<ResponseData>() {
             @Override
             public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
-               if(ResponseCode.isSuccess(response.body())){
-                    ViewUtils.dismiss();
-                };
+                if (ResponseCode.isSuccess(response.body())) {
+                    getUserAddressListRequest();
+                }
+                ;
             }
 
             @Override
             public void onFailure(Call<ResponseData> call, Throwable t) {
-                    ViewUtils.dismiss();
-                    ViewUtils.toast("删除失败");
+                ViewUtils.dismiss();
+                ViewUtils.toast("删除失败");
             }
         });
     }
