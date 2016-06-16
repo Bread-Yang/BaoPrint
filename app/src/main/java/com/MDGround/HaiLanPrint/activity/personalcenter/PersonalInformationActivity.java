8eff78d3b73cb2d687ad6f70c583f70181b166ef
vic5816 <836380339@qq.com>
2016-06-16 13:28:55 +0800

    ....

--- app/src/main/java/com/MDGround/HaiLanPrint/activity/personalcenter/PersonalInformationActivity.java
+++ app/src/main/java/com/MDGround/HaiLanPrint/activity/personalcenter/PersonalInformationActivity.java
@@ -158,7 +158,7 @@ public class PersonalInformationActivity extends ToolbarActivity<ActivityPersona
                 uploadAvatar(Picturepath);
             } else if (requestCode == SelectSingleImageDialog.PHOTO_REQUEST_CAREMA) {// 从相机返回的数据
                 KLog.e("相机返回数据");
-                String Picturepath = Environment.getExternalStorageDirectory() + "/textphoto.jpg";
+                String Picturepath = Environment.getExternalStorageDirectory() + Constants.PHOTO_FILE + "/" + Constants.PHOTO_NAME;
                 uploadAvatar(Picturepath);
             }
 
